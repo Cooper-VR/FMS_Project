338688243f5b06afd0e9ee67f2d6a6c7987999e5
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-10-08 14:17:55 -0700

    made main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('main.js');
+
+function button1(){
+    console.log('button1');
+}
+
+function button2(){
+    console.log('button2');
+}
+
+function button3(){
+    console.log('button3');
+}
+
+function button4(){
+    console.log('button4');
+}
+
+function button5(){
+    console.log('button5');
+}

72f7a3a498857b8aee7f2266a90239e361ac0061
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-10-17 13:23:45 -0700

    Update sketch.js

--- gameScreens/Game2/sketch.js
+++ gameScreens/Game2/sketch.js
@@ -1,43 +1,88 @@
 time = 0;
 
-let positionThreshold = 500;
+let score = 0;
+let misses = 3;
+let speed = 4;
+let playerSize = 20;
+let frutSize = 15;
+let missesString = "";
+let scoreString = "";
+let positionThreshold = (window.innerHeight * 0.9);
 let removeIndexes = -1;
 let positionsArray = [];
 let positionsArray2 = [];
+let gameStarted = false;
+
+let GameOver = false;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight * 0.9);
+  textSize(50);
+  text("Press space to start", window.innerWidth / 2, window.innerHeight / 2);
+}
+
+//start game when space is pressed
+function keyPressed(){
+  if (keyCode === 32){
+    gameStarted = true;
+  }
+}
+
+while (!gameStarted){
+  textSize(50);
+  text("Press space to start", window.innerWidth / 2, window.innerHeight / 2);
 }
 
 function draw() {
-  background(220);
+  if (!GameOver){
+    background(220);
 
-  time += deltaTime / 1000;
+    time += deltaTime / 1000;
 
-  if (time > 3){
-    positionsArray.push(random(0, window.innerWidth));
-    positionsArray2.push(50);
-    time = 0;
-  }
-
-  for (let i = 0; i < positionsArray.length; i++){
-    circle(positionsArray[i], positionsArray2[i], 10);
-    positionsArray2[i] += 1;
-
-    if (positionsArray2[i] > positionThreshold){
-      removeIndexes = i;
+    if (time > 3){
+      positionsArray.push(random(0, window.innerWidth));
+      positionsArray2.push(50);
+      time = 0;
     }
-  }
 
-  if (removeIndexes > -1){
-    positionsArray.shift();
-    positionsArray2.shift();
-    removeIndexes = -1;
-  }
+    for (let i = 0; i < positionsArray.length; i++){
+      circle(positionsArray[i], positionsArray2[i], frutSize);
+      positionsArray2[i] += speed + deltaTime / 1000;
+
+      let dist = Math.sqrt(Math.pow(positionsArray[i] - mouseX, 2) + Math.pow(positionsArray2[i] - ((window.innerHeight * 0.9)-20), 2));
+
+      if (dist < (playerSize + frutSize) / 2){
+        console.log('hit');
+        score++;
+        removeIndexes = i;
+      }
+
+      if (positionsArray2[i] > positionThreshold){
+        misses--;
+        removeIndexes = i;
+      }
+    }
+
+    if (removeIndexes > -1){
+      positionsArray.shift();
+      positionsArray2.shift();
+      removeIndexes = -1;
+    }
+
+    GameOver = (misses == 0)? true : false;
 
 
   //circle is place holder basket
-  circle(mouseX, (window.innerHeight * 0.9)-20, 20);
+    circle(mouseX, (window.innerHeight * 0.9)-20, playerSize);
+    scoreString = "Score: " + score;
+    missesString = "Misses left: " + misses;
+
+    textSize(20);
+    text(scoreString, 50, 50);
+    text(missesString, 50, 70);
+  }
+  textSize(50);
+  text("Game Over", window.innerWidth / 2, window.innerHeight / 2);
 }
 
 //i could spawn them after a time theshold is exceeded
